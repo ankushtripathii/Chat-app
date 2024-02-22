31ff95c79acb0d043dd7fc1969103e0c90369f9f
ankush t <ankushtripathi510@gmail.com>
2024-02-22 15:37:40 +0530

    fetch the postment request

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,27 +1,35 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    fullName: {
-        type: 'string',
-        required: true
+const userSchema = new mongoose.Schema(
+    {
+        fullName: {
+            type: String,
+            required: true,
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+        },
+        gender: {
+            type: String,
+            required: true,
+            enum: ["male", "female"],
+        },
+        profilePic: {
+            type: String,
+            default: "",
+        },
+        // createdAt, updatedAt => Member since <createdAt>
     },
-    userName: {
-        type: 'string',
-        required: true,
-        unique: true
-    },
-    password: {
-        type: 'string',
-        required: true,
-        minlength: 8
-    },
-    gender: {
-        type: 'string',
-        required: true,
-        enum: ["male", "female"]
-    },
-    profilepic: {
-        type: 'string',
-        default: ''
-    },
-})
+    { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,25 @@ import authRoutes from "./routes/auth.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-    // root route http://localhost:5000/
-    res.send("Hello World!");
-
-});
-
 //middleware 
+app.use(express.json()); // to parse the incoming request with JSON payload (from req.body)
+
+
 app.use("/api/auth", authRoutes)
+
+
+
+// app.get('/', (req, res) => {
+//     // root route http://localhost:5000/
+//     res.send("Hello World!");
+
+// });
+
+
 
 app.listen(PORT, () => {
     connectToMongoDB();
