af4f75934fc6e351f2a6eca22965872cab69ca5a
ankush t <ankushtripathi510@gmail.com>
2024-03-13 21:32:32 +0530

    some changes

---
+++ frontend/src/components/sidebar/LogoutButtoon.jsx
@@ -0,0 +1,11 @@
+import { BiLogout } from "react-icons/bi";
+
+const LogoutButtoon = () => {
+  return (
+    <div className="mt-auto">
+      <BiLogout />
+    </div>
+  );
+};
+
+export default LogoutButtoon;
--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,13 +1,14 @@
 import Conversations from "./Conversations";
 import SearchInput from "./SearchInput";
+import LogoutButton from "./LogoutButtoon";
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className="border-r border-slate-500 p-4 flex flex-col">
       <SearchInput />
       <div className="divider px-3"></div>
       <Conversations />
-      {/* <LogoutButton /> */}
+      <LogoutButton />
     </div>
   );
 };
